99fef19d5f879137f2ef2cb2dcb7f89f6ad0f65b
h136799711 <hebiduhebi@126.com>
2015-07-09 15:36:10 +0800

    init

---
+++ bluebao/Common/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  bluebao
+//
+//  Created by hebidu on 15/7/9.
+//  Copyright (c) 2015年 Boye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ bluebao/Common/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  bluebao
+//
+//  Created by hebidu on 15/7/9.
+//  Copyright (c) 2015年 Boye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
